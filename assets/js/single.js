c115b285a5b93b0d6f1cbd3584af2a6588db0a14
Zach Davis <zacharydavis92@gmail.com>
2022-02-01 15:55:46 -0600

    now dynamicly displys repos from searched usernames

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,7 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -20,8 +22,13 @@ var getRepoIssues = function(repo) {
             alert("There was a problem with you request!");
         }
     })
+};
 
-    console.log(repo);
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
 };
 
 var displayIssues = function(issues) {
@@ -70,4 +77,5 @@ var displayWarning = function(repo) {
     // append to warning comtainer
     limitWarningEl.appendChild(linkEl);
 }
-getRepoIssues("facebook/react");
+
+getRepoName();
